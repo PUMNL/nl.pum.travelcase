b10a2ac71dd760d6ce694f8d9c2b42c6248ab86a
ruudgithub <ruudgithub@users.noreply.github.com>
2021-05-26 15:05:05 +0200

    Fixed #5876: Set default status of pre travel check on new case

--- CRM/Travelcase/Utils/SetDefaultValues.php
+++ CRM/Travelcase/Utils/SetDefaultValues.php
@@ -1,19 +1,20 @@
 <?php
 
 class CRM_Travelcase_Utils_SetDefaultValues {
-  
+
   public static function buildForm($formName, &$form) {
     if ($formName != 'CRM_Case_Form_Case') {
       return;
     }
-    
+
     self::setDefaultCaseStatus($form);
     self::setDefaultParentCase($form);
   }
-  
+
   protected static function setDefaultCaseStatus(&$form) {
     $config = CRM_Travelcase_TravelCaseStatusConfig::singleton();
     $defaultStatus = -1; //to be arranged
+    $defaults['custom_'.$config->getCustomFieldPreTravelCheck('id').'_-1'] = $defaultStatus;
     $defaults['custom_'.$config->getCustomFieldPickup('id').'_-1'] = $defaultStatus;
     $defaults['custom_'.$config->getCustomFieldTicket('id').'_-1'] = $defaultStatus;
     $defaults['custom_'.$config->getCustomFieldVisa('id').'_-1'] = $defaultStatus;
@@ -22,7 +23,7 @@ class CRM_Travelcase_Utils_SetDefaultValues {
     $defaults['custom_'.$config->getCustomFieldInvitation('id').'_-1'] = $defaultStatus;
     $form->setDefaults($defaults);
   }
-  
+
   protected static function setDefaultParentCase(&$form) {
     $parent_case_id = CRM_Utils_Request::retrieve('parent_case_id', 'Positive', $form);
     if (!empty($parent_case_id)) {
@@ -37,7 +38,7 @@ class CRM_Travelcase_Utils_SetDefaultValues {
       } catch (Exception $ex) {
           //do nothing
       }
-      
+
       $form->setDefaults($defaults);
     }
   }
@@ -94,5 +95,5 @@ class CRM_Travelcase_Utils_SetDefaultValues {
       return FALSE;
     }
   }
-  
+
 }
